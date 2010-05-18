465cd66eaa8e072c42baffba189c6618a6bc91f0
Vo Minh Thu <noteed@gmail.com>
2010-05-19 00:02:36 +0200

    Added simple program to try Indent.hs.

---
+++ indent.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Language.Syntactical.Indent (go', flatten)
+
+echo :: String -> String
+echo s = case go' s of
+  Right a -> unwords $ flatten a ["\n"]
+  Left err -> "error: " ++ show err ++ "\n"
+
+main :: IO ()
+main = interact echo
+
