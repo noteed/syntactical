adef5b70c8f5b0bdde2d3591c7e03098b95f5fa2
Vo Minh Thu <noteed@gmail.com>
2010-07-10 01:39:47 +0200

    Export closed_ in Syntactical.hs.

--- Text/Syntactical.hs
+++ Text/Syntactical.hs
@@ -1,12 +1,12 @@
 -- Convenience module exporting what's important.
 module Text.Syntactical
   ( shunt, Failure(..)
-  , infx, prefx, postfx, closed, buildTable
+  , infx, prefx, postfx, closed, closed_, buildTable
   , Associativity(..), Kind(..), Table, Tree(..)
   ) where
 
 import Text.Syntactical.Yard
   (shunt, Failure(..))
 import Text.Syntactical.Data
-  (infx, prefx, postfx, closed, buildTable
+  (infx, prefx, postfx, closed, closed_, buildTable
   , Associativity(..), Kind(..), Table, Tree(..))
