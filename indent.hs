162ddfa287ef2f75c9f486dec2fc813057f8ad34
Vo Minh Thu <thu@hypered.io>
2023-07-14 15:00:29 +0200

    Add missing export.

--- indent.hs
+++ indent.hs
@@ -1,9 +1,6 @@
 {-# Language OverloadedStrings #-}
 module Main where
 
-import Test.Framework (defaultMain, testGroup, Test)
-import Test.Framework.Providers.HUnit
-import Test.HUnit hiding (Test)
 import System.Environment (getArgs)
 import Text.ParserCombinators.Parsec
 
