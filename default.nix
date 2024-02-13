909c3eac84e11a7b344015615923b81252c1b308
Vo Minh Thu <thu@hypered.io>
2024-02-13 13:59:57 +0100

    Introduce a nix/ directory.

---
+++ default.nix
@@ -0,0 +1,23 @@
+let
+  sources = import ./nix/sources.nix;
+  overlays = import ./nix/overlays.nix;
+  nixpkgs = import sources.nixpkgs { inherit overlays; };
+
+in rec
+  {
+    # Build with nix-build -A <attr>
+    binaries = nixpkgs.haskellPackages.syntactical;
+    # binaries + haddock are also available as binaries.all.
+    haddock = nixpkgs.haskellPackages.syntactical.doc;
+
+    # A shell to try out our binaries
+    # Run with nix-shell default.nix -A shell
+    shell = nixpkgs.mkShell {
+      buildInputs = [
+        binaries
+      ];
+      shellHook = ''
+        # source <(syntactical --bash-completion-script `which syntactical`)
+      '';
+    };
+  }
---
+++ nix/contents.nix
@@ -0,0 +1,30 @@
+let
+
+  sources = import ./sources.nix;
+  defNixpkgs = import sources.nixpkgs { };
+  nix-filter = import sources.nix-filter;
+
+in { nixpkgs ? defNixpkgs }:
+
+let inherit (nixpkgs.lib.attrsets) getAttrFromPath;
+in {
+  # Lists all packages made available through this nix project.
+  # The format is `{ <pkgName> : <pkgDir> }` (we refer to this as pInfo).
+  # The used directory should be the path of the directory relative to the root
+  # of the project.
+  pkgList = {
+    syntactical = nix-filter {
+      root = ../.;
+      include = with nix-filter; [
+        "syntactical.cabal"
+        "LICENSE"
+        (and "bin" (or_ (matchExt "hs") isDirectory))
+        (and "Text" (or_ (matchExt "hs") isDirectory))
+        (and "tests" (or_ (matchExt "hs") isDirectory))
+      ];
+    };
+  };
+
+  # Get an attribute from a string path from a larger attrSet
+  getPkg = pkgs: pPath: getAttrFromPath [pPath] pkgs;
+}
