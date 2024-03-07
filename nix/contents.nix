36a592fa458a7fd9f3508b4ffc50f71f765148f2
Vo Minh Thu <thu@hypered.io>
2024-03-07 12:32:32 +0100

    Move the ourOverrides function to contents.nix.
    This will make it easier to re-use it when we want to use
    horizon-platform (and in particular, the extend function to add our
    package to its packages, instead of using an overlay).

--- nix/contents.nix
+++ nix/contents.nix
@@ -6,8 +6,9 @@ let
 
 in { nixpkgs ? defNixpkgs }:
 
-let inherit (nixpkgs.lib.attrsets) getAttrFromPath;
-in {
+let
+  inherit (nixpkgs.lib.attrsets) getAttrFromPath mapAttrs;
+
   # Lists all packages made available through this nix project.
   # The format is `{ <pkgName> : <pkgDir> }` (we refer to this as pInfo).
   # The used directory should be the path of the directory relative to the root
@@ -24,7 +25,17 @@ in {
       ];
     };
   };
+in {
+  inherit pkgList;
 
   # Get an attribute from a string path from a larger attrSet
   getPkg = pkgs: pPath: getAttrFromPath [pPath] pkgs;
+
+  # The overriding function that can provide our package to e.g. an overlay.
+  overrides = selfh: superh:
+    let
+      callCabalOn = name: dir:
+        selfh.callCabal2nix "${name}" dir { };
+
+    in mapAttrs callCabalOn pkgList;
 }
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,18 +4,10 @@ let
   lib = super.lib;
   sources = import ./sources.nix;
   contents = import ./contents.nix { nixpkgs = super; };
-  inherit (super.lib.attrsets) mapAttrs;
-
-  ourOverrides = selfh: superh:
-    let
-      callCabalOn = name: dir:
-        selfh.callCabal2nix "${name}" dir { };
-
-    in mapAttrs callCabalOn contents.pkgList;
 
 in {
   haskellPackages = super.haskellPackages.override (old: {
     overrides =
-      lib.composeExtensions (old.overrides or (_: _: { })) ourOverrides;
+      lib.composeExtensions (old.overrides or (_: _: { })) contents.overrides;
   });
 }
